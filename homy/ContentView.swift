e9aabc4b6f59f318eb0953660ec247f481566af9
Jurre Dijkstra <jurredijkstra@mijnheem.nl>
2025-08-26 11:49:10 +0200

    update ContentView.swift

--- homy/ContentView.swift
+++ homy/ContentView.swift
@@ -2,17 +2,25 @@ import SwiftUI
 
 struct ContentView: View {
     @AppStorage("WelcomeScreenShown") private var WelcomeScreenShown = false
+    @State private var selectedTab = 0
     var body: some View {
         if !WelcomeScreenShown {
             WelcomeView()
         } else {
-            MainView() {
+            TabView(selection: $selectedTab) {
+            NavigationView {
                 VStack {
                     Image(systemName: "globe")
                     Text("Hello World!")
                         .font(.title)
                 }
                 .padding()
+            }
+            .tabItem {
+                Image(systemName: "house.fill")
+                Text("Home")
+            }
+            .tag(0)
             }
         }
     }
