5795e5d3ec8b56f7557ee69a24c830ebb56a0e61
Jurre Dijkstra <jurredijkstra@mijnheem.nl>
2025-09-09 22:50:02 +0200

    update App.swift

--- homy/App.swift
+++ homy/App.swift
@@ -6,7 +6,7 @@ struct MyApp: App {
     // Create the ModelContainer for SwiftData models and inject into environment
     let modelContainer: ModelContainer = {
         do {
-            return try ModelContainer(for: [Device.self, Entity.self])
+            return try ModelContainer(for: Device.self, Entity.self)
         } catch {
             fatalError("Failed to create ModelContainer: \(error)")
         }
